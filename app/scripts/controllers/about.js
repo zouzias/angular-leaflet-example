fa303037b379df0f77bc86883bcdffdce9e61caf
Anastasios Zouzias <zouzias@gmail.com>
2015-10-02 20:47:15 +0200

    Init commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularLeafletExampleApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularLeafletExampleApp
+ */
+angular.module('angularLeafletExampleApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularLeafletExampleApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularLeafletExampleApp
+ */
+angular.module('angularLeafletExampleApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
